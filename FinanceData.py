bfdf3d15b18640ca9e941e67382cc44025931a2a
jaychowcl <jaychowcl@gmail.com>
2025-02-11 04:29:59 +0000

    rsi done.

--- FinanceData.py
+++ FinanceData.py
@@ -38,7 +38,10 @@ class FinanceData:
 
     def generateMetrics(self, 
                         metrics = ["sma", "ema", "macd", "adx", "rsi"],
-                        metricTimePeriod = "50d" ): #generate statistics and metrics to append to ticker dataframes
+                        metricTimePeriod = "50d",
+                        macdParams = [12, 26, 9], # fast, slow, signal
+                        rsiParams = 14 #how long RSI period is
+                        ): #generate statistics and metrics to append to ticker dataframes
         self.metricTimePeriod = metricTimePeriod
         numeric_period = int(str(metricTimePeriod).replace("d","")) #TODO: need to make it so d can be any interval
 
@@ -46,16 +49,37 @@ class FinanceData:
         for ticker in self.tickers: #iterate through each ticker
             tickerdf_i = self.tickerDf[ticker] 
 
-            for metric in metrics: #iterate through each metric and check if need to calculate
+            if "sma" in metrics: #simple moving average 
+                tickerdf_i[("sma" + str(metricTimePeriod))] = tickerdf_i["Close"].rolling(window = metricTimePeriod).mean()
+                print("smadone")
 
-                if metric == "sma": #simple moving average 
-                    tickerdf_i[("sma" + str(metricTimePeriod))] = tickerdf_i["Close"].rolling(window = metricTimePeriod).mean()
-                    print("smadone")
+            if "ema" in metrics: #exponential moving average
+                tickerdf_i[("ema" + str(metricTimePeriod))] = tickerdf_i["Close"].ewm(span = numeric_period, adjust=False).mean()
+        
+            if "macd" in metrics: #moving average convergence/divergence: trend following momentum indicator
+                macdFast = tickerdf_i["Close"].ewm(span = macdParams[0], adjust= False).mean()
+                macdSlow = tickerdf_i["Close"].ewm(span = macdParams[1], adjust= False).mean()
 
-                elif metric == "ema": #exponential moving average
-                    tickerdf_i[("ema" + str(metricTimePeriod))] = tickerdf_i["Close"].ewm(span = numeric_period, adjust=False).mean()
-            # print(tickerdf_i)
+                tickerdf_i["macd"] = macdFast - macdSlow
+                tickerdf_i["macd_signal"] = tickerdf_i["macd"].ewm(span = macdParams[2], adjust= False).mean()
+                tickerdf_i["macd_diff"] = tickerdf_i["macd"] - tickerdf_i["macd_signal"]
+            
+            if "rsi" in metrics: #relative strength index: momentum oscillator to measure speed and magnitude of recent prices changes compared to period
+                delta = tickerdf_i["Close"].diff()
+                gains = delta.clip(lower = 0)
+                losses = -1 * delta.clip(upper = 0)
 
+                #calculate averages. use ewm instead of wilder smoothing to achieve basically the same result
+                avgGain = gains.ewm(alpha=1/rsiParams, adjust = False).mean()
+                avgLoss = losses.ewm(alpha=1/rsiParams, adjust = False).mean()
+
+                rs = avgGain/avgLoss#relative strength
+                rsi = 100 - (100 / (1+rs))
+
+                tickerdf_i["rsi"] = rsi
+                print(rsi)
+
+            
 
     def generatePlots(self,
                       cols = ["sma"],
@@ -69,7 +93,10 @@ class FinanceData:
         plotvars = {}
         for col in cols:
             plotvars["Close"] = plotdf["Close"] 
-            plotvars[f"{col}{self.metricTimePeriod}"] = plotdf[f"{col}{self.metricTimePeriod}"]
+            if col in ["sma", "ema"]:
+                plotvars[f"{col}{self.metricTimePeriod}"] = plotdf[f"{col}{self.metricTimePeriod}"]
+            else:
+                plotvars[f"{col}"] = plotdf[f"{col}"]
         plotvars = pd.DataFrame(plotvars,
                                  index = plotdf.index)
         plotvars.plot()
@@ -95,8 +122,10 @@ timeInterval = "1d"
 #data.generateMetrics
 metrics = ["sma", "ema", "macd", "adx", "rsi"]
 metricTimePeriod = "50d"
+macdParams = [12, 26, 9] # 12 fast, 26 slow, 9 signal
+rsiParams = 14
 #data.generatePlots
-plotCols = ["sma", "ema"]
+plotCols = ["sma", "ema", "macd", "macd_signal"]
 plotTicker = None
 
 ####### PIPELINE #######
@@ -115,7 +144,9 @@ data.downloadData()
 
 #generate statistics and metrics
 data.generateMetrics(metrics = metrics,
-                     metricTimePeriod = metricTimePeriod)
+                     metricTimePeriod = metricTimePeriod,
+                     macdParams= macdParams,
+                     rsiParams=rsiParams)
 
 #generate plots
 data.generatePlots(cols = plotCols,
