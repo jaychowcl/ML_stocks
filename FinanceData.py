bddfb65a8a6b60ec323e03d4c85a898cdd4251bd
jaychowcl <jaychowcl@gmail.com>
2025-02-11 16:53:13 +0000

    logistic done. now need to just test logistic prediction. THen need to implement kfold cross validation

--- FinanceData.py
+++ FinanceData.py
@@ -5,7 +5,10 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 from IPython.display import display
+
 from sklearn.model_selection import TimeSeriesSplit
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
 
 ####### CLASS DEFINITIONS #######
 
@@ -48,11 +51,17 @@ class FinanceData:
         self.metricTimePeriod = metricTimePeriod
         numeric_period = int(str(metricTimePeriod).replace("d","")) #TODO: need to make it so d can be any interval
 
-        self.cutTail = max(macdParams + [rsiParams, adxParams])
+        self.cutTail = max(macdParams + [rsiParams, adxParams]) # identify period that need to be cut due to insufficient past data TODO: cut these periods from data
+
 
 
         for ticker in self.tickers: #iterate through each ticker
             tickerdf_i = self.tickerDf[ticker] 
+
+            #calculate delta and %delta
+            tickerdf_i["delta"] = self.tickerDf[ticker]["Close"].diff()
+            tickerdf_i["deltaPerc"] = 100 * (tickerdf_i["delta"] / tickerdf_i["Close"].copy().shift(1))
+
 
             if "sma" in metrics: #simple moving average 
                 tickerdf_i[("sma" + str(metricTimePeriod))] = tickerdf_i["Close"].rolling(window = metricTimePeriod).mean()
@@ -127,6 +136,10 @@ class FinanceData:
                 tickerdf_i["-di"] = diMinus
 
 
+            #trim tail of dataset to remove NaN metrics and clean
+            tickerdf_i.drop(tickerdf_i.index[:self.cutTail], inplace = True)
+
+
     def generatePlots(self,
                       cols = ["sma"],
                       plotTicker = None,
@@ -156,29 +169,43 @@ class FinanceData:
             sns.heatmap(corrmatrix, annot = True)
         
     
-    def kFoldTimeSeries(self,
+    def kFoldSplitTimeSeries(self,
                         kSplits= 5,
                         maxTrainSize = None,
                         testSize = None,
                         gapSize = 0,
                         predictionPeriod = 1, # how many invervals in future that classifier will predict direction on
-                        tickerKFold = None
+                        tickerKFold = None,
+                        yTarget = "targetDirection"
                         ): #split data for kfold cross validation using TimeSeriesSplit
         
         if tickerKFold == None:
             tickerKFold = self.tickers[0]
 
         #get target values according to predictionPeriod
-        tickerdf_i = self.tickerDf[tickerKFold].copy()
-        tickerdf_i["target"] = tickerdf_i["Close"].shift(predictionPeriod)
+        tickerdf_i = self.tickerDf[tickerKFold]
+        tickerdf_i["target"] = tickerdf_i["deltaPerc"].copy().shift(predictionPeriod)
+        tickerdf_i["targetDirection"] = [1 if x >=0 else -1 for x in tickerdf_i["target"]]
+
+        #make targetDirection the target if needed for binary classifiers
+        if yTarget == "targetDirection":
+            tickerdf_i["target"] = tickerdf_i["targetDirection"]
+
+        #remove first row to remove NaN target since will always be in future
+        tickerdf_i.drop(tickerdf_i.index[:1], inplace = True)
+
 
         y = tickerdf_i["target"] # target is close price after specific predictionPeriod
-        X = tickerdf_i.drop(columns=["Close", "High", "Low", "Open"]) # predicotrs are everything except these cols
+        X = tickerdf_i.drop(columns=["Close", "High", "Low", "Open", "target", "targetDirection"]) # predicotrs are everything except these cols
 
         tss = TimeSeriesSplit(n_splits = kSplits, max_train_size=maxTrainSize, test_size=testSize, gap=gapSize)#init tss from scikit learn
 
         #now split and store indexes in self.kFoldIdx
-        self.kFoldIdx = enumerate(tss.split(X))
+        #store split indexes into k:(train, test) dict
+        self.kFoldIdx = {}
+        for i, (trainIdx, testIdx) in enumerate(tss.split(X)):
+            self.kFoldIdx[i] = {"trainIdx": trainIdx, "testIdx": testIdx}
+
         self.kFoldX = X
         self.kFoldy = y
         
@@ -187,6 +214,56 @@ class FinanceData:
         #     print(i)
         #     print(x)
         #     print(y)
+
+    def runStrategy(self, 
+                    strategy = ["logistic"],
+                    X_train = None,
+                    y_train = None,
+                    X_test = None,
+                    y_test = None
+                    ): #input training data and testing predictors to output prediction
+        
+        if X_train is None or y_train is None or X_test is None:
+            raise ValueError("ERROR: Please specify training and testing datasets")
+
+        #scale data to mean 0 and var 1
+        scaler = StandardScaler()
+        scaler.fit(X_train) #train scaler with training predictors
+
+        #scale data for relevant methods
+        X_train_Scale = scaler.transform(X_train)
+        X_test_Scale = scaler.transform(X_test)
+
+        if "logistic" in strategy: # logistic regression with L2 ridge regression regularization and liblinear solver
+            print("Logistic regression:")
+            lr = LogisticRegression(fit_intercept= True, 
+                                    solver = "liblinear", 
+                                    penalty = "l2") # first init logistic regression model
+            lr.fit(X_train_Scale, y_train) # train model
+
+            #use lr model to predict test cases
+            y_predict = lr.predict(X_test_Scale)
+
+            if y_test is None: # return only predictions if without y test
+                return {"y_predict": y_predict, "scores": "NA"}
+            else: # return predictions and accuracy score with y test
+                y_predict_score = lr.score(X_test_Scale, y_test)
+                return {"y_predict": y_predict, "scores": y_predict_score}
+            
+    
+    def kFoldValidation(self,
+                        ):
+        pass
+
+
+
+
+
+        
+
+        
+
+        
 
 
 
@@ -212,13 +289,20 @@ pairplot = 0
 #data.generatePlots
 plotCols = ["sma", "ema", "macd", "macd_signal", "rsi", "adx"]
 plotTicker = None
-#data.kFoldTimeSeries
+#data.kFoldSplitTimeSeries
 kSplits = 5
 maxTrainSize = None
 testSize = None
 gapSize = 0
 predictionPeriod = 1
 tickerKFold = None
+yTarget = "targetDirection"
+#data.runStrategy
+strategy = ["logistic"]
+X_train = None
+y_train = None
+X_test = None
+y_test = None
 
 ####### PIPELINE #######
 
@@ -247,11 +331,22 @@ data.generatePlots(cols = plotCols,
                    pairplot = pairplot)
 
 #split data via TimeSeriesSplit
-data.kFoldTimeSeries(kSplits = kSplits,
-                     maxTrainSize = maxTrainSize,
-                     testSize= testSize,
-                     gapSize=gapSize,
-                     predictionPeriod= predictionPeriod,
-                     tickerKFold = None)
+data.kFoldSplitTimeSeries(kSplits = kSplits,
+                          maxTrainSize = maxTrainSize,
+                          testSize= testSize,
+                          gapSize=gapSize,
+                          predictionPeriod= predictionPeriod,
+                          tickerKFold = None,
+                          yTarget= yTarget)
+
+#run a strategy and predict/evaluate target
+
+data.runStrategy(strategy = strategy,
+                 X_train = data.kFoldX.iloc[data.kFoldIdx[0]["trainIdx"]],
+                 y_train = data.kFoldy.iloc[data.kFoldIdx[0]["trainIdx"]],
+                 X_test = data.kFoldX.iloc[data.kFoldIdx[0]["testIdx"]],
+                 y_test = data.kFoldy.iloc[data.kFoldIdx[0]["testIdx"]]
+                 )
+
 
 ###################################
