c9ab8b0cbda230960beb0de18f619d2fb64c43c1
jaychowcl <jaychowcl@gmail.com>
2025-02-11 12:21:06 +0000

    added kfold splitting. Now need to implement classification strategies

--- FinanceData.py
+++ FinanceData.py
@@ -3,8 +3,9 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-from IPython.display import display
 
+from IPython.display import display
+from sklearn.model_selection import TimeSeriesSplit
 
 ####### CLASS DEFINITIONS #######
 
@@ -47,13 +48,14 @@ class FinanceData:
         self.metricTimePeriod = metricTimePeriod
         numeric_period = int(str(metricTimePeriod).replace("d","")) #TODO: need to make it so d can be any interval
 
+        self.cutTail = max(macdParams + [rsiParams, adxParams])
+
 
         for ticker in self.tickers: #iterate through each ticker
             tickerdf_i = self.tickerDf[ticker] 
 
             if "sma" in metrics: #simple moving average 
                 tickerdf_i[("sma" + str(metricTimePeriod))] = tickerdf_i["Close"].rolling(window = metricTimePeriod).mean()
-                print("smadone")
 
             if "ema" in metrics: #exponential moving average
                 tickerdf_i[("ema" + str(metricTimePeriod))] = tickerdf_i["Close"].ewm(span = numeric_period, adjust=False).mean()
@@ -154,16 +156,38 @@ class FinanceData:
             sns.heatmap(corrmatrix, annot = True)
         
     
-    def kFoldTimeSeries(k = 5,
+    def kFoldTimeSeries(self,
+                        kSplits= 5,
                         maxTrainSize = None,
                         testSize = None,
-                        gapSize = 0):
+                        gapSize = 0,
+                        predictionPeriod = 1, # how many invervals in future that classifier will predict direction on
+                        tickerKFold = None
+                        ): #split data for kfold cross validation using TimeSeriesSplit
         
+        if tickerKFold == None:
+            tickerKFold = self.tickers[0]
 
+        #get target values according to predictionPeriod
+        tickerdf_i = self.tickerDf[tickerKFold].copy()
+        tickerdf_i["target"] = tickerdf_i["Close"].shift(predictionPeriod)
+
+        y = tickerdf_i["target"] # target is close price after specific predictionPeriod
+        X = tickerdf_i.drop(columns=["Close", "High", "Low", "Open"]) # predicotrs are everything except these cols
+
+        tss = TimeSeriesSplit(n_splits = kSplits, max_train_size=maxTrainSize, test_size=testSize, gap=gapSize)#init tss from scikit learn
+
+        #now split and store indexes in self.kFoldIdx
+        self.kFoldIdx = enumerate(tss.split(X))
+        self.kFoldX = X
+        self.kFoldy = y
         
+        # print(self.kFoldIdx)
+        # for i, (x, y) in self.kFoldIdx:
+        #     print(i)
+        #     print(x)
+        #     print(y)
 
-
-        print("end")
 
 
 ###################################
@@ -184,15 +208,17 @@ metricTimePeriod = "50d"
 macdParams = [12, 26, 9] # 12 fast, 26 slow, 9 signal
 rsiParams = 14
 adxParams =14
-pairplot = 1
+pairplot = 0
 #data.generatePlots
 plotCols = ["sma", "ema", "macd", "macd_signal", "rsi", "adx"]
 plotTicker = None
 #data.kFoldTimeSeries
-k = 5
+kSplits = 5
 maxTrainSize = None
 testSize = None
 gapSize = 0
+predictionPeriod = 1
+tickerKFold = None
 
 ####### PIPELINE #######
 
@@ -221,9 +247,11 @@ data.generatePlots(cols = plotCols,
                    pairplot = pairplot)
 
 #split data via TimeSeriesSplit
-data.kFoldTimeSeries(k = k,
+data.kFoldTimeSeries(kSplits = kSplits,
                      maxTrainSize = maxTrainSize,
                      testSize= testSize,
-                     gapSize=gapSize)
+                     gapSize=gapSize,
+                     predictionPeriod= predictionPeriod,
+                     tickerKFold = None)
 
 ###################################
